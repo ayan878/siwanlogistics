b47586d3df4c069661e7bb648e45055867c056f1
ayan878 <ayanraza197@gmail.com>
2024-01-23 02:04:59 +0530

    swn

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+export function Home() {
+  return(
+  <div>
+   <Header/>
+  </div>
+  )
+}
+function Header() {
+  return (
+    <div>
+      <div className="Logo-Header">
+        <h3>SiwanLogistics</h3>
+      </div>
+    </div>
+  );
+}

195b76de2a15b0e88be30a4519383f1f6762863e
ayan878 <ayanraza197@gmail.com>
2024-01-23 17:00:34 +0530

    swn

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { IoCallOutline, IoMailOutline, IoTimeOutline } from "react-icons/io5";
 import "./Home.css";
+
 export function Home() {
   return (
     <div>
@@ -7,35 +9,40 @@ export function Home() {
     </div>
   );
 }
+
 function Header() {
   return (
     <div className="header">
       <div className="logo-header">
         <h3>
-          <img src="./logo.png" />
+          <img src="./logo.png" alt="Logo" />
           TransitFlow
         </h3>
       </div>
       <div className="address-bar">
-        <div className="address">
-          <IoTimeOutline className="icon" />
-          <p>
-            Monday-Sat 9:00-18:00 <br />
-            Sunday Closed
-          </p>
-        </div>
-
-        <div className="address">
-          <IoMailOutline className="icon" />
-          <p>Email <br/>example@logistics.com</p>
-        </div>
-        <div className="address">
-          <IoCallOutline className="icon" />
-          <p>
-            Call Us <br /> +1-354863114
-          </p>
-        </div>
+        <Address
+          icon={<IoTimeOutline />}
+          text="Monday-Sat 9:00-18:00\nSunday Closed"
+        />
+        <Address icon={<IoMailOutline />} text="Email\nexample@logistics.com" />
+        <Address icon={<IoCallOutline />} text="Call Us\n+1-354863114" />
       </div>
+    </div>
+  );
+}
+
+function Address({ icon, text }) {
+  return (
+    <div className="address">
+      {icon && <div className="icon">{icon}</div>}
+      <p>
+        {text.split('\n').map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
+      </p>
     </div>
   );
 }

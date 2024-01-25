12955723af8178f958dc3fb68542aa50f8e486ae
ayan878 <ayanraza197@gmail.com>
2024-01-25 12:49:10 +0530

    swn

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,21 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import { IoCallOutline, IoMailOutline, IoTimeOutline } from "react-icons/io5";
-import { Link } from 'react-router-dom'; 
 
-import "./Home.css";
+const addressData = [
+  {
+    label: "Opening Hours",
+    value: "Monday-Sat 9:00-18:00\nSunday Closed",
+    icon: <IoTimeOutline />,
+  },
+  { label: "Email", value: "example@logistics.com", icon: <IoMailOutline /> },
+  { label: "Call Us", value: "+1-354863114", icon: <IoCallOutline /> },
+];
 
 export function Home() {
   return (
     <div>
       <Header />
-      <Menu/>
     </div>
   );
 }
@@ -23,23 +30,26 @@ function Header() {
         </h3>
       </div>
       <div className="address-bar">
-        <Address
-          icon={<IoTimeOutline />}
-          text="Monday-Sat 9:00-18:00\nSunday Closed"
-        />
-        <Address icon={<IoMailOutline />} text="Email\nexample@logistics.com" />
-        <Address icon={<IoCallOutline />} text="Call Us\n+1-354863114" />
+        {addressData.map((address, index) => (
+          <Address
+            key={index}
+            label={address.label}
+            value={address.value}
+            icon={address.icon}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
-function Address({ icon, text }) {
+function Address({ label, value, icon }) {
   return (
     <div className="address">
       {icon && <div className="icon">{icon}</div>}
-      <p>
-        {text.split('\n').map((line, index) => (
+      <p className="address-text">
+        <strong>{label}</strong>:
+        {value.split("\n").map((line, index) => (
           <React.Fragment key={index}>
             {line}
             <br />
@@ -49,29 +59,29 @@ function Address({ icon, text }) {
     </div>
   );
 }
-function Menu(){
-  return (
-    <div className="menu">
-      <nav className="navbar">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about">Pages</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about">Project</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
 
+// function Menu(){
+//   return (
+//     <div className="menu">
+//       <nav className="navbar">
+//         <ul className="nav-list">
+//           <li className="nav-item">
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link to="/about">Pages</Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link to="/about">Project</Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link to="/contact">Contact</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Home';
+import './Home.css';
 import App from './App';
 
 
